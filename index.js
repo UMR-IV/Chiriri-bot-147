1f82c270f51f4ec126f68350fe5e285aeba46ca1
UMR_IV <70298796+UMR-IV@users.noreply.github.com>
2021-04-24 23:35:35 +0800

    Update index.js

--- index.js
+++ index.js
@@ -28,14 +28,7 @@ client.on('message', message =>{
          message.channel.send('<a:UMRPat:805597149509320734>')
          message.react('<a:UMRPat:805597149509320734>')
         
-    } else if(command == 'youtube'){                                        //change message input here
-            if(message.member.roles.cache.has('664063399092813834'){
-                message.channel.send('https://cdn.discordapp.com/attachments/792676622939455512/806168433749131344/Wife2.mp4')
-            } else if(message.member.roles.cache.has('769820874353147944')){
-                message.channel.send('https://cdn.discordapp.com/attachments/792676622939455512/806168433749131344/Wife2.mp4')
-            } else{
-                message.channel.send('No permission')
-    }
+    } 
 });
 
 client.login(process.env.DJS_TOKEN)
