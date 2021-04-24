1b7125255666af59e583d0303257d33e4a0ea83c
UMR_IV <70298796+UMR-IV@users.noreply.github.com>
2021-04-24 23:32:01 +0800

    Update index.js

--- index.js
+++ index.js
@@ -24,21 +24,17 @@ client.on('message', message =>{
     const command = args.shift().toLowerCase();
     
     if(command === 'ping'){
-        execute(message, args){
          message.channel.send('<@!664063399092813834>')
          message.channel.send('<a:UMRPat:805597149509320734>')
          message.react('<a:UMRPat:805597149509320734>')
-        }
+        
     } else if(command == 'youtube'){                                        //change message input here
-        execute(message, args){
             if(message.member.roles.cache.has('664063399092813834')){
                 message.channel.send('https://cdn.discordapp.com/attachments/792676622939455512/806168433749131344/Wife2.mp4')
             } else if(message.member.roles.cache.has('769820874353147944')){
                 message.channel.send('https://cdn.discordapp.com/attachments/792676622939455512/806168433749131344/Wife2.mp4')
             } else{
                 message.channel.send('No permission')
-            }
-        
     }
 });
 
