2751aab40bb50662e7c59db703c1231a178c8aae
unknown <umaralhadi47@gmail.com>
2021-04-24 21:07:00 +0800

    Update online log identifier

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const client = new Discord.Client()
 //const config = require('./config.json')
 
 client.on('ready', () =>{
-    console.log('The client is ready!')
+    console.log('Chiriri is online!')
 })
 
 client.login(process.env.DJS_TOKEN)
