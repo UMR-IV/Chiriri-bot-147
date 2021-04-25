f466dcf8fd44723a808e1133932d20ecf282573e
UMR_IV <70298796+UMR-IV@users.noreply.github.com>
2021-04-25 12:52:48 +0800

    Add files via upload

---
+++ welcome.js
@@ -0,0 +1,14 @@
+module.exports = client => {
+
+    const channelID = "835444751494807593" //testing channel
+
+
+    client.on('GuildMemberAdd', member =>{
+        console.log(member)
+    })
+
+    const message = `Welcome to the server <@${member.id}>, I will ping <@${664063399092813834}> for you`
+
+    const channel = member.guild.channels.cache.get(channelId)
+    channel.send(message)
+}
