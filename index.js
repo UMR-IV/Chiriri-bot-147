75e789d8627fc762e4a6416418b04a7ffc44431e
UMR_IV <70298796+UMR-IV@users.noreply.github.com>
2021-04-25 08:51:17 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
 
+const command = require('./command')
 
+//=============================================
 client.on('ready', () =>{
     console.log('Chiriri is online!')
-})
 
+    command(client, 'ping', message =>{
+        message.channel.send('Pong!')
+    })
+})
+//=============================================
+
+
+
+//=============================================
 client.login(process.env.DJS_TOKEN)
