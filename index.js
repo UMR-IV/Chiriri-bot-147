771af461901b13674b6c904bbb0d5ec51d04e892
UMR_IV <70298796+UMR-IV@users.noreply.github.com>
2021-04-25 10:00:21 +0800

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ client.on('ready', () =>{
 
     command(client, ['cc', 'clearchannel'], message =>{
         if (message.member.hasPermission('ADMINISTRATOR')){
-            message.channel.message.fetch().then((results) =>{
+            message.channel.messages.fetch().then((results) =>{
                 message.channel.bulkDelete(results)
             })
         }
