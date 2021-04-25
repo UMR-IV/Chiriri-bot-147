52dfd9c86127dd5424259d85b42df63a4fcacde2
UMR_IV <70298796+UMR-IV@users.noreply.github.com>
2021-04-25 13:04:55 +0800

    Update welcome.js

--- welcome.js
+++ welcome.js
@@ -1,12 +1,12 @@
 module.exports = client => {
 
-    const channelID = "835444751494807593" //testing channel
+    //const channelID = "835444751494807593" //testing channel
     client.on('GuildMemberAdd', (member) =>{
         console.log(member)
     })
-
-    const message = `Welcome to the server <@${member.id}>, I will ping for you`
+/*
+    const message = `Welcome to the server <@${member.id}>, I will ping <@${664063399092813834}> for you`
 
     const channel = member.guild.channels.cache.get(channelId)
-    channel.send(message)
+    channel.send(message)*/
 }
