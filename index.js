0da6a7fdd813912e53ecde6652b5e725fd42337e
UMR_IV <70298796+UMR-IV@users.noreply.github.com>
2021-04-25 09:22:07 +0800

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ client.on('ready', () =>{
 
     command(client, 'servers', message =>{
         client.guilds.cache.forEach(guild =>{
-            console.log(guild)
+            message.channel.send(`${guild.name} has a total of ${guild.memberCount} members`)
         })
     })
 })
