e72442b645b7d0ae7c7d6fd7c51a28dfeb6a3f3e
UMR_IV <70298796+UMR-IV@users.noreply.github.com>
2021-04-25 12:41:08 +0800

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ client.on('ready', () =>{
 
     command(client, ['ping', 'test'], message =>{
         message.channel.send(`${message.author.toString()} Pong!`)
-        message.console.log(channelID)
+        console.log(channelID)
     })
 
     command(client, 'help', message =>{
