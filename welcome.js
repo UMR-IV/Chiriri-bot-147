95ca9119c4c6f16a52ab1ca75bb41148a583e012
UMR_Hungry <70298796+UMR-IV@users.noreply.github.com>
2021-05-21 10:01:09 +0800

    Update welcome.js

--- welcome.js
+++ welcome.js
@@ -5,7 +5,7 @@ module.exports = (client) => {
         console.log(member)
 
         //const message = `Welcome to the server <@${member.id}> I will ping <@664063399092813834> for you`
-        const message = `Selamat datang ke Langit <@${member.id}> ! <a838780271641624617>`
+        const message = `Selamat datang ke Langit <@${member.id}> ! <a:NanaSmugGlasses:838780271641624617>`
 
         const channel = member.guild.channels.cache.get(channelId)
         channel.send(message)
