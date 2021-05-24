90c6582ae14a2f5b14bc5c237682cbd270c0a399
UMR_Hungry <70298796+UMR-IV@users.noreply.github.com>
2021-05-24 14:37:57 +0800

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ client.on('ready', () =>{
     
     command(client, ['ping', 'test'], message =>{
         message.channel.send(`${message.author.toString()} Pong!`)
-        message.react(':smile:')
+        message.channel.send(':smile:')
     })
 
     command(client, 'help', message =>{
