cbae97a06078a884cafb8715df5cadab91fdd513
UMR_147 【ウマル】 <70298796+UMR-IV@users.noreply.github.com>
2022-10-02 16:09:17 +0800

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const command = require('./command.js')
 const welcome = require('./welcome.js')
 //=============================================
 client.on('ready', () =>{
-    console.log('Chiriri is online!')
+    console.log('Chiriri is online lah!')
 
     welcome(client)
     
